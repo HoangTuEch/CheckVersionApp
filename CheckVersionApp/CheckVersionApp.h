cf79abb8d2626ad827b9b60600ba6b3926fcafa4
DatND <datnd2@rikkeisoft.com>
2022-07-26 09:29:33 +0700

    Initial Commit

---
+++ CheckVersionApp/CheckVersionApp.h
@@ -0,0 +1,18 @@
+//
+//  CheckVersionApp.h
+//  CheckVersionApp
+//
+//  Created by DatND2 on 7/26/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckVersionApp.
+FOUNDATION_EXPORT double CheckVersionAppVersionNumber;
+
+//! Project version string for CheckVersionApp.
+FOUNDATION_EXPORT const unsigned char CheckVersionAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckVersionApp/PublicHeader.h>
+
+
